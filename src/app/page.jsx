70b38bf3bdfd8dd835321803890e43b3354217fd
Tejash2911 <tcpatel2911@gmail.com>
2024-05-10 22:48:02 +0530

    create layout and page

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Announcement from "@/components/Announcement";
+import Navbar from "@/components/Navbar";
+import Slider from "@/components/Slider";
+
+export default function Home() {
+  return (
+    <main>
+      <Announcement />
+      <Navbar />
+      <Slider />
+    </main>
+  );
+}
