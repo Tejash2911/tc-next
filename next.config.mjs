1302e14dd867675db7c176ea87b6384ff8f187fd
Tejash2911 <tcpatel2911@gmail.com>
2024-05-10 22:47:14 +0530

    initialize next js project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'themanufacturer-cdn-1.s3.eu-west-2.amazonaws.com',
+                port: '',
+                pathname: '/wp-content/**'
+            },
+        ],
+    },
+};
 
 export default nextConfig;
