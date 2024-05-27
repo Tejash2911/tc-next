19710a3845fe4f277c92fbb9557798658116b1f4
Tejash2911 <tcpatel2911@gmail.com>
2024-05-28 00:50:43 +0530

    add product not found and empty cart

---
+++ src/components/EmptyCart.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function EmptyCart() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-2 font-Urbanist my-5">
+      <Image src="/cart.svg" alt="not-found" width={900} height={600} className="w-1/5" />
+      <h3 className="text-3xl font-semibold text-center">Your cart is empty</h3>
+      <span className="text-xl font-extralight text-center">Looks like you haven&apos;t added anything to cart yet</span>
+      <Link href="/">
+        <button className="p-2 w-[150px] text-sm  border border-teal-700 bg-white transition-all duration-300 hover:bg-teal-700 hover:text-white">Go Home</button>
+      </Link>
+    </div>
+  );
+}
---
+++ src/components/ProductNotFound.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function ProductNotFound(props) {
+  return (
+    <div className="flex flex-col justify-center items-center font-Urbanist">
+      <Image src="/itemNf.png" alt="not-found" width={1348} height={900} className="w-1/4" />
+      <h3 className="text-3xl my-2 text-center">{props.title}</h3>
+      <span className="text-xl font-extralight text-center">{props.desc}</span>
+      <span className="text-xl font-extralight text-center">Please Try Again</span>
+    </div>
+  );
+}
