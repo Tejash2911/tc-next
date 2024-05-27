54d65f694d7d1cea56586abed2c7777694b4e0c8
Tejash2911 <tcpatel2911@gmail.com>
2024-05-28 00:52:03 +0530

    add message component

---
+++ src/components/Message.jsx
@@ -0,0 +1,38 @@
+"use client";
+import { clearError } from "@/lib/features/error/errorSlice";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { useEffect, useState } from "react";
+import CancelIcon from "@mui/icons-material/Cancel";
+
+export default function Message() {
+  const dispatch = useAppDispatch();
+  const id = useAppSelector((state) => state.error.id);
+  const message = useAppSelector((state) => state.error.error);
+  const [isShow, setIsShow] = useState(false);
+
+  useEffect(() => {
+    if (message && id) {
+      setIsShow(true);
+      const timeout = setTimeout(() => {
+        dispatch(clearError());
+        setIsShow(false);
+      }, 5000);
+      return () => clearTimeout(timeout);
+    }
+  }, [message, id, dispatch]);
+
+  return (
+    <>
+      {isShow && (
+        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 animate-fadeInOut font-Urbanist">
+          <div className="bg-gradient-to-r from-[#001845] via-[#022b3a] to-[#00384a] text-white rounded-2xl shadow-md max-w-sm w-full p-3">
+            <span className="mx-2">{message}</span>
+            <button onClick={() => setIsShow(false)} className="text-white hover:text-red-500 focus:outline-none">
+              <CancelIcon />
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
