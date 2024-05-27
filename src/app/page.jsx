e87d4d5734a532be8187f8ba4b14a8e39cd015cc
Tejash2911 <tcpatel2911@gmail.com>
2024-05-28 00:42:42 +0530

    add dynamic import

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,19 @@
-import Announcement from "@/components/Announcement";
-import Navbar from "@/components/Navbar";
-import Slider from "@/components/Slider";
+import dynamic from "next/dynamic";
+import Category from "@/components/Category";
+
+const DynamicSlider = dynamic(() => import("../components/Slider"), {
+  loading: () => <p>Slider Loading...</p>,
+});
+const DynamicProduct = dynamic(() => import("../components/Product"), {
+  loading: () => <p>Product Loading...</p>,
+});
 
 export default function Home() {
   return (
     <main>
-      <Announcement />
-      <Navbar />
-      <Slider />
+      <DynamicSlider />
+      <Category />
+      <DynamicProduct />
     </main>
   );
 }
