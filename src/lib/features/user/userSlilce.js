7471b0ad4589a990ff32830cb99ce31b8bc947ce
Tejash2911 <tcpatel2911@gmail.com>
2024-05-28 00:48:15 +0530

    save user and address to localstorage

--- src/lib/features/user/userSlilce.js
+++ src/lib/features/user/userSlilce.js
@@ -1,61 +1,68 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+  address: null,
+  isFetching: false,
+  isError: false,
+  error: null,
+};
+
+if (typeof window !== "undefined") {
+  const storedUser = localStorage.getItem("user");
+  initialState.currentUser = storedUser ? JSON.parse(storedUser) : null;
+}
+
+if (typeof window !== "undefined") {
+  const storedUserAddress = localStorage.getItem("address");
+  initialState.address = storedUserAddress ? JSON.parse(storedUserAddress) : null;
+}
 
 const userSlice = createSlice({
-    name: "user",
-    initialState: {
-        currentUser: null,
-        address: null,
-        isFetching: false,
-        isError: false,
-        error: null
+  name: "user",
+  initialState,
+  reducers: {
+    logoutUser: (state) => {
+      state.currentUser = null;
+      localStorage.removeItem("user");
+      state.address = null;
     },
-    reducers: {
-        //default
-        Start: (state) => {
-            state.isError = false;
-            state.isFetching = true;
-            state.error = null;
-        },
-        logoutUser: (state) => {
-            state.currentUser = null;
-            state.address = null;
-        },
-        resetError: (state) => {
-            state.error = null;
-            state.isError = null;
-        },
-        setAddress: (state, action) => {
-            state.address = action.payload;
-        },
-        updateUser: (state, action) => {
-            state.currentUser = { ...state.currentUser, ...action.payload }
-        },
+    resetError: (state) => {
+      state.error = null;
+      state.isError = null;
+    },
+    setAddress: (state, action) => {
+      state.address = action.payload;
+      localStorage.setItem("address", JSON.stringify(action.payload));
+    },
+    updateUser: (state, action) => {
+      state.currentUser = { ...state.currentUser, ...action.payload };
+    },
 
-        //login
-        loginSucces: (state, action) => {
-            state.isFetching = false;
-            state.currentUser = action.payload;
-            state.isError = false;
-            console.log(action.payload)
-        },
-        Failed: (state, action) => {
-            state.isFetching = false;
-            state.isError = action.payload;
-        },
+    //login
+    loginSucces: (state, action) => {
+      state.isFetching = false;
+      state.currentUser = action.payload;
+      localStorage.setItem("user", JSON.stringify(action.payload));
+      state.isError = false;
+    },
+    Failed: (state, action) => {
+      state.isFetching = false;
+      state.isError = action.payload;
+    },
 
-        //signup
-        signUpSucces: (state, action) => {
-            state.isFetching = false;
-            state.isError = false;
-            state.currentUser = action.payload;
-        },
-        signupFailed: (state, action) => {
-            state.isFetching = false;
-            state.error = action.payload;
-        },
-
-    }
-})
+    //signup
+    signUpSucces: (state, action) => {
+      state.isFetching = false;
+      state.isError = false;
+      state.currentUser = action.payload;
+    },
+    signupFailed: (state, action) => {
+      state.isFetching = false;
+      state.error = action.payload;
+    },
+  },
+});
 
 export const { Start, loginSucces, Failed, logoutUser, signUpSucces, signupFailed, resetError, setAddress, updateUser } = userSlice.actions;
 export default userSlice.reducer;
