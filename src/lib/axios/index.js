27c65294afad2031faf18af7766f436fa87fa237
Tejash2911 <tcpatel2911@gmail.com>
2024-10-02 20:53:28 +0530

    CHANGE: BASE URL

--- src/lib/axios/index.js
+++ src/lib/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const BASE_URL = 'https://tc-api-4sbm.onrender.com/api/v1'
-const BASE_URL = 'http://localhost:4000/api/v1'
+const BASE_URL = 'https://tc-api-4sbm.onrender.com/api/v1'
+// const BASE_URL = 'http://localhost:4000/api/v1'
 
 function getAccessToken() {
   try {
